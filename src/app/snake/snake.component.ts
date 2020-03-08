eac85b9b536d8314fcb84ab8f9d91d66df087683
Wuhang <yanwuhang@gmail.com>
2020-03-08 22:43:13 +0800

    make snake move

--- src/app/snake/snake.component.ts
+++ src/app/snake/snake.component.ts
@@ -7,11 +7,19 @@ import { snakeBody, snakeBodyStyles } from '../../model/snake-body.model';
   styleUrls: ['./snake.component.css']
 })
 export class SnakeComponent implements OnInit {
-  public body: snakeBody = [];
-  public styles: snakeBodyStyles = [];
+  public body: snakeBody[] = [];
+  public styles: snakeBodyStyles[] = [];
+  public direction: string;
+  public height: number;
+  public width: number;
+  public timer: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.direction = 'right';
+    this.height = 20;
+    this.width = 20;
     this.body = [
       {
         x: 3,
@@ -26,23 +34,63 @@ export class SnakeComponent implements OnInit {
         y: 1
       }
     ];
-    this.styles = [
-      {
-        'backgroundColor': 'red',
-        'top': '20px',
-        'left': '60px'
-      },
-      {
-        'backgroundColor': 'grey',
-        'top': '20px',
-        'left': '40px'
-      },
-      {
-        'backgroundColor': 'grey',
-        'top': '20px',
-        'left': '20px'
+    this.generateSnake();
+    this.timer = window.setInterval(() => { this.move() }, 800);
+  }
+
+  public generateSnake() {
+    this.styles = [];
+    for (let i in this.body) {
+      /* generate snake head */
+      if (i === '0') {
+        this.styles.push(
+          {
+            'backgroundColor': 'red',
+            'top': this.height * this.body[0].y + 'px',
+            'left': this.width * this.body[0].x + 'px'
+          },
+        )
       }
-    ]
+      else {
+        this.styles.push(
+          {
+            'backgroundColor': 'grey',
+            'top': this.height * this.body[i].y + 'px',
+            'left': this.width * this.body[i].x + 'px'
+          },
+        )
+      }
+    }
+  }
+
+  public move() {
+    for (let i = this.body.length - 1; i > 0; i--) {
+      this.body[i].x = this.body[i - 1].x;
+      this.body[i].y = this.body[i - 1].y;
+    }
+    switch (this.direction) {
+      case 'right': this.body[0].x++;
+        break;
+      case 'left': this.body[0].x--;
+        break;
+      case 'up': this.body[0].y++;
+        break;
+      case 'down': this.body[0].y--;
+        break;
+      default:
+        break;
+    }
+    this.checkLoose();
+
+  }
+
+  public checkLoose() {
+    if (this.body[0].x === 0 || this.body[0].x === 35 || this.body[0].y === 0 || this.body[0].y === 35) {
+      clearInterval(this.timer);
+    }
+    else {
+      this.generateSnake();
+    }
   }
 
 }
